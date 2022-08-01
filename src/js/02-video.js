dd7c5c6e1b27a8e566a6dae980a08c9e84280313
Taras <jogoogidi@gmail.com>
2022-08-01 14:03:11 +0300

    1403

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,11 +6,18 @@ const iframeEl = document.querySelector('#vimeo-player');
 const playerEl = new Vimeo(iframeEl);
 const seconds = JSON.parse(localStorage.getItem(CURRENT_TIME));
 
-playerEl.setCurrentTime(seconds.seconds);
+timeRed()
+
 
 playerEl.on(`timeupdate`, throttle(timeRec, 1000));
 
 function timeRec(currentTime) {
     localStorage.setItem(CURRENT_TIME, JSON.stringify(currentTime))
+    console.log(currentTime)
 };
 
+function timeRed() {
+    if (seconds) {
+    playerEl.setCurrentTime(seconds.seconds);
+}
+};
