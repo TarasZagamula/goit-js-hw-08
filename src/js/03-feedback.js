0d59f0ceb3cd9874248636d1b706e2e0fb49b885
Taras <jogoogidi@gmail.com>
2022-08-05 01:03:47 +0300

    0103

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,53 @@
 import throttle from "lodash.throttle";
 
 const inputForm = document.querySelector(`.feedback-form`);
-const formData = {};
 const STORAGE_KEY = "feedback-form-state";
+const saved = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const formData = {
+    email: "",
+    message: "",
+};
 
-messageRec();
+if (saved !== null) {
+ messageParse();
+};
+
 
 inputForm.addEventListener(`input`, throttle(onInputChange, 500));
 inputForm.addEventListener(`submit`, onFormSubmit);
 
 function onInputChange(e) {
     const formValue = e.target.value;
-    if (formValue) {
-        formData[e.target.name] = formValue;
-        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-        console.log(formData);
+    
+    if (e.target.name === `email`) {
+        formData.email = formValue; 
+        formData.message = saved ? saved.message : "";
+    } else {
+        formData.message = formValue;
+        formData.email = saved ? saved.email : "";
     }
+    messageRec(formData);
 };
 
-function messageRec() {
-    
-    const saved = JSON.parse(localStorage.getItem(STORAGE_KEY));
+function messageRec(el) {
+    return localStorage.setItem(STORAGE_KEY, JSON.stringify(el));
+};
+
+function messageParse() {
         if (saved) {
-        inputForm.elements.email.value = saved.email || "";
-        inputForm.elements.message.value = saved.message || "";
-    
+        inputForm.elements.email.value = saved.email;
+        inputForm.elements.message.value = saved.message;
     }
 };
 
 function onFormSubmit(e) {
     e.preventDefault();
-    if (e.target.value === undefined) {
-        return alert(`Заполните все поля!`);
-    }
-    localStorage.removeItem(STORAGE_KEY);
+    if (inputForm.elements.email.value || inputForm.elements.message.value !== "") {
+    localStorage.removeItem(STORAGE_KEY)
     e.target.reset();
-
+    return
+    } 
+        
+    alert(`Заполните все поля!`);
+    console.log(`test`)
 };
